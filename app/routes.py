0e5261693c6c324655539e1385316a49bcb54fab
malan88 <malan88@standingwater.io>
2018-07-06 17:38:41 -0400

    Added template support

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index/')
 def index():
-    return "Hello, world!"
+    hello = 'Hello, World!'
+    return render_template('index.html', title=hello)
