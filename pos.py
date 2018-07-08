c5877ff3c1233ae50739b478b3479758d6b0a26f
malan88 <malan88@standingwater.io>
2018-07-07 21:37:27 -0400

    fixed pos.py

--- pos.py
+++ pos.py
@@ -6,7 +6,7 @@ with open(sys.argv[1], 'rt') as fin, open(sys.argv[2], 'wt') as fout:
     i = 0
     for line in fin:
         if '@' in line:
-            fout.write(f'{str(i)}@{book}@{line}')
+            fout.write(str(book) + '@' + str(i) + '@' + line[:-1] + '\n')
         else:
-            fout.write(f'{str(i)}@{book}@{line}@0')
+            fout.write(str(book) + '@' + str(i) + '@' + line[:-1] + '@0' + '\n')
         i += 1
