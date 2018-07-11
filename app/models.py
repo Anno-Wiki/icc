51fc8c933fd1f68760bdb619b50d50eac51edd0b
malan88 <malan88@standingwater.io>
2018-07-11 15:15:49 -0400

    Figured out foreign key relationships, got them working

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return "<User {}>".format(self.username)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -24,54 +24,61 @@ class User(UserMixin, db.Model):
 def load_user(id):
     return User.query.get(int(id))
 
+
+
+
 class Word(db.Model):
     # Columns
     id = db.Column(db.Integer, primary_key = True)
-    word = db.Column(db.VARCHAR(128, collation='utf8mb4_bin'), unique=True, index = True)
-
-    # Relationships
-    typesetting = db.relationship('Position', backref = 'neverquery', lazy = 'dynamic')
+    word = db.Column(db.VARCHAR(128, collation="utf8mb4_bin"), unique=True, index = True)
 
     def __repr__(self):
-        return '<Word {}>'.format(self.word)
+        return "<Word {}>".format(self.word)
 
 class Position(db.Model):
     # Columns
     id = db.Column(db.Integer, primary_key = True)
-    book = db.Column(db.Integer, db.ForeignKey('book.id'), index = True)
+
+    book_id = db.Column(db.Integer, db.ForeignKey("book.id"), index = True)
+    book = db.relationship("Book", backref="typesetting")
+
     position = db.Column(db.Integer, index = True)
-    word = db.Column(db.Integer, db.ForeignKey('word.id'), index = True)
+
+    word_id = db.Column(db.Integer, db.ForeignKey("word.id"), index = True)
+    word = db.relationship("Word")
+
     number = db.Column(db.Integer, index = True)
 
     def __repr__(self):
-        return f'<Position {self.position} of {self.book}: {self.word}'
+        return f"<Position {self.position} of {self.book.title}: {self.word.word}>"
 
 class Book(db.Model):
     # Columns
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(128))
-    author = db.Column(db.Integer, db.ForeignKey('author.id'), index = True)
+
+    author_id = db.Column(db.Integer, db.ForeignKey("author.id"), index = True)
+    author = db.relationship("Author", backref="books")
+
     published = db.Column(db.Date)
+
     ts_added = db.Column(db.DateTime, default = datetime.utcnow)
 
-    # Relationships
-    positions = db.relationship('Position', backref = 'typesetting', lazy = 'dynamic')
-
     def __repr__(self):
-        return f'<Book: {self.title} by {self.author}>'
+        return f"<Book: {self.title} by {self.author}>"
 
 class Author(db.Model):
     # Columns
     id = db.Column(db.Integer, primary_key = True)
+
     name = db.Column(db.String(128), index = True)
     first_name = db.Column(db.String(128), index = True)
     last_name = db.Column(db.String(128), index = True)
+
     birth_date = db.Column(db.Date, index = True)
     death_date = db.Column(db.Date, index = True)
+
     ts_added = db.Column(db.DateTime, index = True, default = datetime.utcnow)
 
-    # Relationships
-    books = db.relationship('Book', backref = 'books', lazy = 'dynamic')
-
     def __repr__(self):
-        return f'<Author: {self.name}>'
+        return f"<Author: {self.name}>"
