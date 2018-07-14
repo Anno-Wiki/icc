9fccf935a2dbe66bf699937e913982414a1f4f7d
malan88 <malan88@standingwater.io>
2018-07-14 09:47:19 -0400

    Created dot leader style, tried to adjust icc.py, created htmlgen.py

--- icc.py
+++ icc.py
@@ -1,6 +1,7 @@
 from app import app, db
-from app.models import User
+from app.models import User, Book, Page, Position, Author
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User}
+    return {'db': db, 'User': User, 'Book': Book, 'Page': Page, 'Position':
+            Position, 'Author': Author}
