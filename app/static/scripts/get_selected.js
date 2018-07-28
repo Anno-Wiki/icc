778f8a436b89f794c04dec7ae7b54e189694da40
malan88 <malan88@standingwater.io>
2018-07-28 17:48:14 -0400

    just debugged get_selected.js a little. Need to commit to rangy

--- app/static/scripts/get_selected.js
+++ app/static/scripts/get_selected.js
@@ -4,24 +4,27 @@ $(document).ready(function(){
     var getAllBetween = function (firstEl,lastEl) {
         var firstElement = $(firstEl); // First Element
         var lastElement = $(lastEl); // Last Element
-        var collection = new Array(); // Collection of Elements
+        var collection = []; // Collection of Elements
         collection.push(firstElement.attr('id')); // Add First Element to Collection
+
         $(firstEl).nextAll().each(function(){ // Traverse all siblings
             var siblingID  = $(this).attr('id'); // Get Sibling ID
-            if (siblingID != $(lastElement).attr('id')) { // If Sib is not LastElement
+            if (siblingID !== $(lastElement).attr('id')) { // If Sib is not LastElement
                 collection.push($(this).attr('id')); // Add Sibling to Collection
             } else { // Else, if Sib is LastElement
                 collection.push(lastElement.attr('id')); // Add Last Element to Collection
                 return false; // Break Loop
             }
-        });         
+        });
+
         return collection; // Return Collection
-    }
+    };
+
 
     var get_selection = function (event){
         if (window.getSelection) { // non-IE
-            userSelection = window.getSelection();
-            rangeObject = userSelection.getRangeAt(0);
+            var userSelection = window.getSelection();
+            var rangeObject = userSelection.getRangeAt(0);
             if (rangeObject.startContainer == rangeObject.endContainer) {
                 return rangeObject.startContainer.parentNode.id;
             } else {
@@ -32,24 +35,24 @@ $(document).ready(function(){
             }
 
         } else if (document.selection) { // IE lesser
-            userSelection = document.selection.createRange();
-            var ids = new Array();
+            var userSelection = document.selection.createRange();
+            var ids = [];
             
-            if (userSelection.htmlText.toLowerCase().indexOf('span') >= 0) {
-                $(userSelection.htmlText).filter('span').each(function(index, span) {
-                    ids.push(span.id);
+            if (userSelection.htmlText.toLowerCase().indexOf('word') >= 0) {
+                $(userSelection.htmlText).filter('word').each(function(index, word) {
+                    ids.push(word.id);
                 });
                 return ids;
             } else {
                 return userSelection.parentElement().id;
             }
         }
-    }
+    };
 
 
 
     document.addEventListener('selectionchange', function() {
-        if(window.getSelection().toString() != ''){
+        if(window.getSelection().toString() !== ''){
             $(".linker").css('visibility', 'visible');
         }
         else{
@@ -60,19 +63,19 @@ $(document).ready(function(){
 
     var modal = $("#linkerbox");
     var btn = $(".linker")[0];
-    var span = $(".close")[0];
+    var word = $(".close")[0];
     btn.onclick = function() {
         $('#selection').text(window.getSelection().toString());
         $('#tags').text(get_selection());
         modal.css('display','block');
-    }
-    span.onclick = function() {
+    };
+    word.onclick = function() {
         modal.css('display','none');
-    }
+    };
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.css('display','none');
         }
-    }
+    };
 
 });
