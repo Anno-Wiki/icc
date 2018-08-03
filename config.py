8f734e4cb6867550b0689f0fc8ed93b66b30ac51
malan88 <malan88@standingwater.io>
2018-08-03 09:43:10 -0400

    Adjusted the mysql key for root w/o pwd

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Swl,Ap,as,atot,alAgb'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://malan:utterAhoySequence~@localhost/icc?charset=utf8mb4'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root@localhost/icc?charset=utf8mb4'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
