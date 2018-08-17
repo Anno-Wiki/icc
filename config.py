fb725fbca414627d4fa84bf900b940a76000840e
malan88 <malan88@standingwater.io>
2018-08-17 11:32:17 -0400

    Minor fix

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Swl,Ap,as,atot,alAgb'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "youllneverguess"
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root@localhost/icc?charset=utf8mb4'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
