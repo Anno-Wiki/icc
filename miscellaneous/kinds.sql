d0533b64dc56e899ce17fa3c5034ad9823017f01
malan88 <malan88@standingwater.io>
2018-08-18 11:45:40 -0400

    Renamed kinds and readied it for new kinds table
    Which is just the old L_class table

--- miscellaneous/kinds.sql
+++ miscellaneous/kinds.sql
@@ -1,3 +1,3 @@
-INSERT INTO L_class (l_class) VALUES 
+INSERT INTO kind (kind) VALUES 
     ('hr'), ('ch'), ('pt'), ('bk'), ('stg'), ('pre'), ('lpre'), ('fpre'),
     ('spre'), ('rsl'), ('rll'), ('rfl'), ('rl'), ('sl'), ('ll'), ('fl'), ('l');
