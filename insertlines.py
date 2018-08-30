e3c544e7cf5d32ffc8b710af451f428a9f255c20
malan88 <malan88@standingwater.io>
2018-08-30 15:21:25 -0400

    Rewrote insertlines to be faster
    I just indexed the kinds in a dict instead of querying every for line

--- insertlines.py
+++ insertlines.py
@@ -5,15 +5,21 @@ import codecs
 
 fin = codecs.getreader('utf_8_sig')(sys.stdin.buffer, errors='replace')
 
+kinds = Kind.query.all()
+kind = {}
+for k in kinds:
+    kind[k.kind] = k.id
+
+
 for line in fin:
     fields = line.split('@')
 
-    k = Kind.query.filter_by(kind = fields[2]).first()
-    e = Kind.query.filter_by(kind = fields[6]).first()
+#    k = Kind.query.filter_by(kind = fields[2]).first()
+#    e = Kind.query.filter_by(kind = fields[6]).first()
 
-    l = Line(book_id = fields[0], l_num = fields[1], kind_id = k.id, 
+    l = Line(book_id = fields[0], l_num = fields[1], kind_id = kind[fields[2]], 
         bk_num = fields[3], pt_num = fields[4], ch_num = fields[5],
-        em_status_id = e.id, line = fields[7])
+        em_status_id = kind[fields[6]], line = fields[7])
 
     db.session.add(l)
 
