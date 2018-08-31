267572c324d567a9e28df2d2267c2496a02da1bc
malan88 <malan88@standingwater.io>
2018-08-31 16:02:35 -0400

    Created LINES_PER_PAGE in config

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "youllneverguess"
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root@localhost/icc?charset=utf8mb4'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LINES_PER_PAGE = 30
