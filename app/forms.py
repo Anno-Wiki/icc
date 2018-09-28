01b226ee24b74b580aa7711e8c66b97c804576a6
malan88 <malan88@standingwater.io>
2018-09-28 10:15:26 -0400

    Restricted tag use blocked

--- app/forms.py
+++ app/forms.py
@@ -69,40 +69,46 @@ class AnnotationForm(FlaskForm):
         tag = Tag.query.filter_by(tag=tag_1.data).first()
         if tag is None:
             raise ValidationError(f"The tag '{tag_1.data}' does not exist.")
+        if tag.admin:
+            raise ValidationError(f"The tag '{tag_1.data}' is restricted.")
 
     def validate_tag_2(self, tag_2):
         tag = Tag.query.filter_by(tag=tag_2.data).first()
         if tag is None:
             raise ValidationError(f"The tag '{tag_2.data}' does not exist.")
-
         if tag_2.data == self.tag_1.data:
             raise ValidationError("You cannot use the same tag twice.")
+        if tag.admin:
+            raise ValidationError(f"The tag '{tag_2.data}' is restricted.")
 
     def validate_tag_3(self, tag_3):
         tag = Tag.query.filter_by(tag=tag_3.data).first()
         if tag is None:
             raise ValidationError(f"The tag '{tag_3.data}' does not exist.")
-
         if tag_3.data == self.tag_1.data or tag_3.data == self.tag_2.data:
             raise ValidationError("You cannot use the same tag twice.")
+        if tag.admin:
+            raise ValidationError(f"The tag '{tag_3.data}' is restricted.")
 
     def validate_tag_4(self, tag_4):
         tag = Tag.query.filter_by(tag=tag_4.data).first()
         if tag is None:
             raise ValidationError(f"The tag '{tag_4.data}' does not exist.")
-
         if tag_4.data == self.tag_1.data or tag_4.data == self.tag_2.data or \
                 tag_4.data == self.tag_3.data:
             raise ValidationError("You cannot use the same tag twice.")
+        if tag.admin:
+            raise ValidationError(f"The tag '{tag_4.data}' is restricted.")
 
     def validate_tag_5(self, tag_5):
         tag = Tag.query.filter_by(tag=tag_5.data).first()
         if tag is None:
             raise ValidationError(f"The tag '{tag_5.data}' does not exist.")
-
         if tag_5.data == self.tag_1.data or tag_5.data == self.tag_2.data or \
                 tag_5.data == self.tag_3.data or tag_5.data == self.tag_4.data:
             raise ValidationError("You cannot use the same tag twice.")
+        if tag.admin:
+            raise ValidationError(f"The tag '{tag_5.data}' is restricted.")
 
 class TagForm(FlaskForm):
     tag = StringField("Tag", render_kw={"placeholder":"Tag"},
