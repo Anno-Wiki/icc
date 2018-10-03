ad5c773138c68f4d7022d23611a8189d1cfd9357
malan88 <malan88@standingwater.io>
2018-10-03 11:14:17 -0400

    Fixed bug in rights.sql

---
+++ sql/rights.sql
@@ -0,0 +1,8 @@
+INSERT INTO admin_right (`right`) VALUES
+    ('edit_locked_annotations'),
+    ('create_tags'),
+    ('use_restricted_tags'),
+    ('lock_user_accounts'),
+    ('lock_annotations'),
+    ('edit_lines'),
+    ('immediate_edits');
