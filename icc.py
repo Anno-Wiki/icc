97ff46c725ba46e86e78c524b4fec383ef341636
malan88 <malan88@standingwater.io>
2018-10-03 11:28:14 -0400

    Added all models to flask shell dictionary

--- icc.py
+++ icc.py
@@ -1,6 +1,13 @@
 from app import app, db
-from app.models import User, Book, Author
+from app.models import AdminRight, User, Vote, EditVote, Book, Author, Line, \
+    Kind, Tag, Annotation, AnnotationVersion
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Book': Book, 'Author': Author} 
+    return {'db': db,
+            'AdminRight': AdminRight, 'User': User,
+            'Vote': Vote, 'EditVote': EditVote,
+            'Book': Book, 'Author': Author,
+            'Line': Line,
+            'Kind': Kind, 'Tag': Tag,
+            'Annotation': Annotation, 'AnnotationVersion': AnnotationVersion }
