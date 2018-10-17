782acdbf7095ff85db2525fce541de10012fdb18
malan88 <malan88@standingwater.io>
2018-10-17 11:12:07 -0400

    Progress on tag.annotations

--- app/models.py
+++ app/models.py
@@ -317,25 +317,14 @@ class Tag(db.Model):
             secondary="join(tags, AnnotationVersion,"
             "tags.c.annotation_version_id==AnnotationVersion.id)",
             primaryjoin="Tag.id==tags.c.tag_id",
-            secondaryjoin="Annotation.head_id==AnnotationVersion.id",
-            lazy="dynamic")
+            secondaryjoin="Annotation.head_id==AnnotationVersion.id")
 
     annotations2 = db.relationship("Annotation",
             secondary="join(tags, AnnotationVersion,"
             "and_(tags.c.annotation_version_id==AnnotationVersion.id,"
             "AnnotationVersion.current==True))",
             primaryjoin="Tag.id==tags.c.tag_id",
-            secondaryjoin="AnnotationVersion.pointer_id==Annotation.id",
-            lazy="dynamic")
-
-#    annotations3 = db.relationship("Annotation",
-#            secondary="join(Tag, tags, Tag.id==tags.c.tag_id)"
-#            ".join(AnnotationVersion, tags,"
-#            "and_(tags.c.annotation_version_id==AnnotationVersion.id,"
-#            "AnnotationVersion.current==True))",
-#            primaryjoin="Annotation.id==AnnotationVersion.pointer_id",
-#            lazy="dynamic")
-
+            secondaryjoin="AnnotationVersion.pointer_id==Annotation.id")
 
     def __repr__(self):
         return f"<Tag {self.id}: {self.tag}>"
