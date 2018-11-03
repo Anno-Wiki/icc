78265057ae5b8caae9a132a7ce8417080026962b
malan88 <malan88@standingwater.io>
2018-11-03 14:26:05 -0400

    Created respectable SO style tag input
    Still not working fully: I need onclick for annotate to repopulate the
    tags input box. lol.

--- app/static/scripts/tags.js
+++ app/static/scripts/tags.js
@@ -1,24 +1,71 @@
 function $(sel) { return document.querySelector(sel); }
 
 window.onload = function () {
-    tags = $("#tags");
-    tags.onkeyup = function () { autocomplete(tags.value); };
+    initial_tag_creation();
 };
 
-function autocomplete(prefix) {
-    var request = new XMLHttpRequest();
-    request.open("POST", "/autocomplete/tags/");
-    request.onload = function () {
-        const data = JSON.parse(request.responseText);
-        if (data.success) {
-            console.log(data);
-        } else {
-            console.log("nope");
+function initial_tag_creation() {
+    var tags = $("#tags");
+    var raw_text = tags.value;
+    if (raw_text != "") {
+        var tags_array = raw_text.split(" ");
+        var div = $("#tag_spans");
+        var array_length = tags_array.length;
+        for (var i = 0; i < array_length; i++) {
+            var tag = document.createElement("tag");
+            tag.innerHTML = tags_array[i] + " &times;";
+            tag.onclick = function() {
+                spans = $("#tag_spans");
+                this.parentNode.parentNode.removeChild(this.parentNode);
+            };
+            div.appendChild(tag);
         }
-    };
-
-    const data = new FormData();
-    data.append("tags", prefix);
-    request.send(data);
+        tags.value = "";
+    }
+    tags.onkeyup = autocomplete;
 };
 
+
+function autocomplete(event) {
+    var key = event.which || event.keyCode;
+    var prefix = $("#tags").value;
+    if (key == 32) {
+        var tag = document.createElement("tag");
+        prefix = prefix.replace(/(^\s+|\s+$)/g, '');
+        tag.innerHTML = prefix + " &times;";
+        tag.onclick = function() {
+            spans = $("#tag_spans");
+            this.parentNode.parentNode.removeChild(this.parentNode);
+        };
+        var div = $("#tag_spans");
+        div.appendChild(tag);
+        $("#tags").value = "";
+    } else if (key == 8 && prefix == "") {
+        var spans = $("#tag_spans");
+        var last = spans.lastChild;
+        if (last) {
+            var text = last.innerHTML;
+            var input = $("#tags");
+            if (text) {
+                text = text.slice(0, -2); 
+                input.value = text;
+            }
+            spans.removeChild(last);
+        }
+    } else {
+        var request = new XMLHttpRequest();
+        request.open("POST", "/autocomplete/tags/");
+        request.onload = function () {
+            const data = JSON.parse(request.responseText);
+            if (data.success) {
+                console.log(data);
+            } else {
+                console.log("nope");
+            }
+        };
+
+        const data = new FormData();
+        data.append("tags", prefix);
+        request.send(data);
+    }
+};
