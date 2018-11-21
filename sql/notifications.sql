b3e76cf8af4488051f2b5e251c1e412e222c7d5a
malan88 <malan88@standingwater.io>
2018-11-21 13:19:08 -0500

    More notifications and rights

--- sql/notifications.sql
+++ sql/notifications.sql
@@ -5,4 +5,9 @@ INSERT INTO notification_type (code, description) VALUES
     ("edit_rejected", "annotation edit rejected"),
     ("new_annotation", "new annotation"),
     ("new_annotation_flag", "new annotation flag"),
-    ("new_user_flag", "new user flag");
+    ("new_user_flag", "new user flag"),
+    ("book_approved", "book request approved"),
+    ("book_rejected", "book request rejected"),
+    ("book_added", "approved book added"),
+    ("tag_approved", "tag approved"),
+    ("tag_rejected", "tag rejected");
