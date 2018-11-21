41c0288982b136372be34dcb816760f349a4491a
malan88 <malan88@standingwater.io>
2018-11-21 16:34:23 -0500

    Converted kinds to lvls

--- sql/kinds.sql
+++ sql/kinds.sql
@@ -1,4 +1,4 @@
 INSERT INTO kind (kind) VALUES 
-    ('hr'), ('ch'), ('pt'), ('bk'), ('stg'), ('pre'), ('lpre'), ('fpre'),
-    ('spre'), ('rsl'), ('rll'), ('rfl'), ('rl'), ('sl'), ('ll'), ('fl'), ('l'),
-    ('oem'), ('cem'), ('nem'), ('em'), ('act'), ('scn'), ('quo');
+    ('hr'), ('lvl1'), ('lvl2'), ('lvl3'), ('lvl4'), ('stg'), ('pre'), ('lpre'),
+    ('fpre'), ('spre'), ('sl'), ('ll'), ('fl'), ('l'), ('oem'), ('cem'),
+    ('nem'), ('em'), ('act'), ('scn'), ('quo');
