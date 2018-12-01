daddac32add068461c475b91d5ad31814095d6af
malan88 <malan88@standingwater.io>
2018-12-01 14:53:51 -0500

    Organized config.py and added a few things

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     ADMINS = ["emails@futuretld.org"]
     ANNOTATIONS_PER_PAGE = 5
-    ANNO_DOWN_FACTOR = 15
-    ANNO_UP_FACTOR = 10
     AUTHORIZATION = { 
             "anonymize_users": -1,
             "approve_edits": -1,
@@ -26,6 +24,7 @@ class Config(object):
             "immediate_edits": -1,
             "lock_annotations": -1,
             "lock_users": -1,
+            "reject_edits": -1,
             "request_books": 100,
             "request_tags": 100,
             "resolve_annotation_flags": -1,
@@ -37,23 +36,31 @@ class Config(object):
             "view_deactivated_annotations": -1,
             }
 
-    CARDS_PER_PAGE = 15
-    LINES_PER_SEARCH_PAGE = 10
-    ANNOTATIONS_PER_SEARCH_PAGE = 5
-    ELASTICSEARCH_URL = os.environ.get("ELASTICSEARCH_URL")
-    LINES_PER_PAGE = 30
+
+    # mail system
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
-    MIN_APPROVAL_RATING = 2
-    MIN_REJECTION_RATING = -2
+
+    # vote margins
+    MIN_EDIT_APPROVAL_RATING = 2
+    MIN_EDIT_REJECTION_RATING = -2
+
+    # per pages
+    ANNOTATIONS_PER_SEARCH_PAGE = 5
+    CARDS_PER_PAGE = 15
+    LINES_PER_PAGE = 30
+    LINES_PER_SEARCH_PAGE = 10
     NOTIFICATIONS_PER_PAGE = 15
+
+    USER_PAGE_AVATAR_SIZE = 255
+
+    # technicals
     SECRET_KEY = os.environ.get("SECRET_KEY") or "youllneverguess"
+    ELASTICSEARCH_URL = os.environ.get("ELASTICSEARCH_URL")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "mysql+pymysql://root@localhost/icc?charset=utf8mb4"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPVOTES_FOR_NOTIFICATION = 2
-    USER_PAGE_AVATAR_SIZE = 255
 #    SQLALCHEMY_ECHO = 1
 #    SERVER_NAME = "www.annopedia.org:5000"
