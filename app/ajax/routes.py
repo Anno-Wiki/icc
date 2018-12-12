57ceb704ee819e24dd6c40e6ba3d83bb1ca26628
malan88 <malan88@standingwater.io>
2018-12-12 18:23:29 -0500

    Fix ajax autocomplete route
    I never changed the `Tag.admin` field to `Tag.locked`

--- app/ajax/routes.py
+++ app/ajax/routes.py
@@ -7,7 +7,7 @@ from . import ajax
 def ajax_tags():
     tagstr = request.form.get("tags")
     tags = tagstr.split()
-    results = Tag.query.filter(Tag.tag.startswith(tags[-1]), Tag.admin==False)\
+    results = Tag.query.filter(Tag.tag.startswith(tags[-1]), Tag.locked==False)\
             .limit(6)
     if not results:
         return jsonify({"success": False, "tags": []})
