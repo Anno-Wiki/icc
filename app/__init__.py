e33d000e3b024ebefea4135b477c6eae3e5ae291
malan88 <malan88@standingwater.io>
2018-12-16 20:14:44 -0500

    Fix login_view since modularization of user blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 md = Markdown(app)
-login.login_view = "login"
+login.login_view = "user.login"
 mail = Mail(app)
 elasticsearch = Elasticsearch([app.config["ELASTICSEARCH_URL"]]) \
         if app.config["ELASTICSEARCH_URL"] else None
