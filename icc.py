f9c69da1679371de3b285b1ef70a5968993f0486
malan88 <malan88@standingwater.io>
2018-12-18 21:10:23 -0500

    Improve shell context creation
    Basically eliminated a lot of time wasting by just importing classes
    from app.models

--- icc.py
+++ icc.py
@@ -1,42 +1,8 @@
 from app import app, db, elasticsearch
-from app.models import Right, User,\
-        Notification, NotificationEnum, NotificationObject,\
-        ReputationChange, ReputationEnum,\
-        UserFlagEnum, UserFlag,\
-        Vote, EditVote,\
-        Text, Edition, Writer,\
-        WriterEditionConnection, ConnectionEnum,\
-        TextRequest, TextRequestVote,\
-        Line, LineEnum,\
-        Annotation, Edit, Comment,\
-        AnnotationFlag, AnnotationFlagEnum,\
-        Tag, TagRequest, TagRequestVote
+from app.models import classes
 
 @app.shell_context_processor
 def make_shell_context():
-    return {"db": db,
-
-            "User": User,
-            "Right": Right,
-            "Notification": Notification, "NotificationEnum": NotificationEnum,
-            "NotificationObject": NotificationObject,
-            "ReputationChange": ReputationChange,
-            "ReputationEnum": ReputationEnum,
-            "UserFlag": UserFlag, "UserFlagEnum": UserFlagEnum,
-
-            "Vote": Vote, "EditVote": EditVote,
-
-            "Text": Text, "Edition": Edition, "Writer": Writer,
-            "WriterEditionConnection": WriterEditionConnection,
-            "ConnectionEnum": ConnectionEnum,
-            "TextRequest": TextRequest, "TextRequestVote": TextRequestVote,
-
-            "Line": Line, "LineEnum": LineEnum, 
-            "Annotation": Annotation, "Edit": Edit, "Comment": Comment,
-            "AnnotationFlag": AnnotationFlag,
-            "AnnotationFlagEnum": AnnotationFlagEnum,
-
-            "Tag": Tag,
-            "TagRequest": TagRequest, "TagRequestVote": TagRequestVote,
-
-            "es" : elasticsearch }
+    classes["db"] = db
+    classes["es"] = elasticsearch
+    return classes
