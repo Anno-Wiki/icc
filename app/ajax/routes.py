4b391b876a86e646de4176821d0ab21b381e428f
malan88 <malan88@standingwater.io>
2018-12-22 12:45:24 -0500

    Converted ajax/ and email/ files to new stylistic convention

--- app/ajax/routes.py
+++ app/ajax/routes.py
@@ -3,19 +3,19 @@ from app import app, db
 from app.models import Tag
 from . import ajax
 
-@ajax.route("/autocomplete/tags/", methods=["POST"])
+@ajax.route('/autocomplete/tags/', methods=['POST'])
 def ajax_tags():
-    tagstr = request.form.get("tags")
+    tagstr = request.form.get('tags')
     tags = tagstr.split()
     results = Tag.query.filter(Tag.tag.startswith(tags[-1]), Tag.locked==False)\
             .limit(6)
     if not results:
-        return jsonify({"success": False, "tags": []})
+        return jsonify({'success': False, 'tags': []})
     tag_list = []
     descriptions = []
     for t in results:
         tag_list.append(t.tag)
         descriptions.append(t.description)
 
-    return jsonify({"success": True, "tags": tag_list,
-        "descriptions": descriptions })
+    return jsonify({'success': True, 'tags': tag_list,
+        'descriptions': descriptions })
--- app/email/email.py
+++ app/email/email.py
@@ -15,12 +15,12 @@ def send_email(subject, sender, recipients, text_body, html_body):
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    send_email("[Annopedia] Reset Your Password",
-            sender=app.config["ADMINS"][0],
+    send_email('[Annopedia] Reset Your Password',
+            sender=app.config['ADMINS'][0],
             recipients=[user.email],
-            text_body=render_template("email/reset_password.txt",
+            text_body=render_template('email/reset_password.txt',
                 user=user, token=token),
-            html_body=render_template("email/reset_password.html",
+            html_body=render_template('email/reset_password.html',
                 user=user, token=token)
             )
 
