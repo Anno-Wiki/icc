2abbb8272126495c2a134ed7d20752866081a0f6
malan88 <malan88@standingwater.io>
2019-01-06 09:25:55 -0500

    Correct bad path in repopulate.sh

--- scripts/repopulate.sh
+++ scripts/repopulate.sh
@@ -8,5 +8,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 sudo echo "- dropping and recreating database..."
 echo "drop database icc; create database icc;" | sudo mysql icc
 echo "- recreating table structure..."
-/home/malan/projects/icc/icc/venv/bin/flask db upgrade
+$DIR/../venv/bin/flask db upgrade
 $DIR/populate.sh $1
