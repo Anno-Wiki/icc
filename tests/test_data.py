930c202f771bcc48bb537bf9243cf2b013b9bee3
malan88 <malan88@standingwater.io>
2019-01-20 09:17:39 -0500

    Rearrange/rename tests.
    I added a few tests. There is a major problem with testing (a possible
    bug) wherein any call to the flask app immediately wipes out
    sqlalchemy's ability to calculate secondary relationships (i.e.,
    relationships requiring a secondary table or a secondary class).
    
    I have constructed a question to help me answer the question why.
    
    I am going to start building a pytest system.

--- tests/test_data.py
+++ tests/test_data.py
@@ -22,14 +22,6 @@ class Test(unittest.TestCase):
     app.config['SQLALCHEMY_ECHO'] = 0
     app.config['WTF_CSRF_ENABLED'] = 0
 
-    # I don't get why at all, but any relationship that requires a secondary
-    # table (or rather, specifically, the `?.annotations lazy="dynamic"`
-    # relationships) don't work in this actual unit. I don't get it
-    # what-so-fucking-ever. I will have to inquire with StackOverflow.
-    # Thankfully, the actual route _does_ work, so I am able to test by actually
-    # setting the number of annotations as a class constant.
-
-
     # Helper functions
     def login(self, email, password):
         with app.app_context():
@@ -42,70 +34,96 @@ class Test(unittest.TestCase):
             url = url_for('user.logout')
         return self.app.get(url, follow_redirects=True)
 
-
-    # setup and tear down functions
-    def setUp(self):
+    # setup function for the entire set of tests
+    @classmethod
+    def setUpClass(cls):
         db.create_all()
-        self.app =  app.test_client()
+
+        # populate all enums
         for enum, enums in data['enums'].items():
             for instance in enums:
                 obj = classes[enum](**instance)
                 if enum == 'User':
                     obj.set_password(password)
                 db.session.add(obj)
+
+        # populate the text
         text = copy.deepcopy(data['text'])
         t = Text(**text)
+        db.session.add(t)
+
+        # populate the authors
         for author in authors:
             t.authors.append(Writer(**author))
+
+        # populate the edition
         e = Edition(text=t, **edition)
-        db.session.add(t)
         db.session.add(e)
-        db.session.commit()
 
-
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-        fin.close()
-
-
-    # partial set up and tear down functions
-    def setup_lines(self):
+        # popoulate the lines
         labels = LineEnum.query.all()
         label = {}
         for l in labels:
-            label[f'{l.label}>{l.display}'] = l
+            label[f'{l.enum}>{l.display}'] = l
         e = Edition.query.first()
         for line in lines:
             db.session.add(Line(edition=e, num=line['num'],
-                label=label[line['label']],
+                label=label[line['enum']],
                 em_status=label[line['em_status']],
                 lvl1=line['l1'], lvl2=line['l2'], lvl3=line['l3'],
                 lvl4=line['l4'], line=line['line']))
-    
 
-    def setup_annotations(self):
-        i = 0
-        e = Edition.query.first()
-        annos = copy.deepcopy(annotations)
-        for a in annos:
+        # populate the annotations
+        for a in annotations:
             annotator = a.pop('annotator')
             annotator = User.query.filter_by(displayname=annotator).first()
             tag_strings = a.pop('tags')
             tags = [Tag.query.filter_by(tag=tag).first() for tag in tag_strings]
             db.session.add(Annotation(annotator=annotator, edition=e, tags=tags,
                 **a))
-            i += 1
-        self.annotations = i
+        db.session.commit()
 
 
+    @classmethod
+    def tearDownClass(cls):
+        db.session.remove()
+        db.drop_all()
+        fin.close()
+
+    # get app context for each test
+    def setUp(self):
+        self.app =  app.test_client()
+
+    def test_secondaries_context(self):
+        text = Text.query.first()
+        self.assertNotEqual(text.annotations.count(), 0) # before app context
+        with app.app_context():
+            pass
+        self.assertNotEqual(text.annotations.count(), 0) # after app context
+
+    def test_secondaries_index(self):
+        text = Text.query.first()
+        self.assertNotEqual(text.annotations.count(), 0) # before app query
+        rv = self.app.get('http://www.testing.com/index')
+        self.assertEqual(rv.status_code, 200)
+        self.assertNotEqual(text.annotations.count(), 0) # before app query
+
+    def test_rights(self):
+        rights = data['enums']['Right']
+        for r in rights:
+            self.assertIsNotNone(Right.query.filter_by(enum=r['enum']).first())
+
+    def test_user_authorization(self):
+        rights = Right.query.all()
+        u = User.query.first()
+        u.rights = rights
+        db.session.commit()
+        for r in rights:
+            self.assertTrue(u.is_authorized(r.enum))
 
     def test_login_logout(self):
         u = User.query.filter_by(locked=False).first()
-        lu = User.query.filter_by(locked=True).first()
 
-        self.assertTrue(b'That account is locked' in self.login(lu.email,
-            password).data)
         self.assertTrue(b'Invalid email or password' in self.login(u.email,
             'bullcrap').data)
 
@@ -117,41 +135,38 @@ class Test(unittest.TestCase):
             rv = self.logout()
             self.assertEqual(rv.status_code, 200)
 
+    def test_locked_login(self):
+        lu = User.query.filter_by(locked=True).first()
+        self.assertTrue(b'That account is locked' in self.login(lu.email,
+            password).data)
 
     def test_annotations(self):
-        self.setup_lines()
-        self.setup_annotations()
-        db.session.commit()
+        annotations = Annotation.query.all()
+        for a in annotations:
+            self.assertTrue(len(a.HEAD.tags) >= 1)
+            self.assertTrue(len(a.HEAD.context) >= 1)
+            self.assertTrue(len(a.HEAD.lines) >= 1)
 
-        #annotations = Annotation.query.all()
-        #for a in annotations:
-        #    self.assertTrue(len(a.HEAD.tags) >= 1)
-        #    self.assertTrue(len(a.HEAD.context) >= 1)
-        #    self.assertTrue(len(a.HEAD.lines) >= 1)
-
-
-    def test_user_funcs(self):
+    def test_password(self):
         u = User(displayname='john', email='john@example.com')
         u.set_password('dog')
         self.assertFalse(u.check_password('cat'))
         self.assertTrue(u.check_password('dog'))
+
+    def test_avatar(self):
+        u = User(displayname='john', email='john@example.com')
         self.assertEqual(u.avatar(128), 
                 ('https://www.gravatar.com/avatar/d4c74594d841139328695756648b6'
                 'bd6?d=identicon&s=128'))
 
-
     def test_index(self):
-        self.setup_annotations()
-        self.setup_lines()
-        db.session.commit()
-
         sorts = ['newest', 'oldest', 'modified', 'weight', 'thisdoesntexist']
         with app.app_context():
             url = url_for("index")
         entities = Annotation.query.count()
         max_pages = int(math.ceil(entities / app.config['ANNOTATIONS_PER_PAGE']))
 
-        rv = self.app.get(f'{url}')
+        rv = self.app.get(url)
         self.assertEqual(rv.status_code, 200)
         for sort in sorts:
             rv = self.app.get(f'{url}?sort={sort}')
@@ -160,7 +175,6 @@ class Test(unittest.TestCase):
             self.assertEqual(rv.status_code, 200)
             rv = self.app.get(f'{url}?sort={sort}&page={max_pages+1}')
             self.assertEqual(rv.status_code, 404)
-
 
     def test_writer_index(self):
         with app.app_context():
@@ -187,9 +201,6 @@ class Test(unittest.TestCase):
         self.assertEqual(rv.status_code, 200)
 
     def test_writer_annotations(self):
-        self.setup_lines()
-        self.setup_annotations()
-        db.session.commit()
         writer = Writer.query.first()
 
         self.assertTrue(writer.annotations.all())
@@ -198,12 +209,7 @@ class Test(unittest.TestCase):
             url = url_for('writer_annotations', writer_url=writer.url)
         sorts = ['newest', 'oldest', 'weight']
 
-        # This is weird as hell, but, while the actual route
-        # `writer_annotations` returns the actual annotations from the
-        # relationship `writer.annotations`, the query (here)
-        # `writer.annotations.all()` and `writer.annotations.count` return
-        # nothing and 0, so we'll just manually set the number of annotations.
-        entities = self.annotations
+        entities = writer.annotations.count()
         max_pages = int(math.ceil(entities / app.config['ANNOTATIONS_PER_PAGE']))
 
         rv = self.app.get(url)
@@ -244,17 +250,16 @@ class Test(unittest.TestCase):
 
 
     def test_text_annotations(self):
-        self.setup_annotations()
-        self.setup_lines()
-        db.session.commit()
-
         text = Text.query.first()
         with app.app_context():
             url = url_for('text_annotations', text_url=text.url)
 
         sorts = ['newest', 'oldest', 'weight', 'line']
 
-        entities = self.annotations
+        # this should return the number of annotations in the entire system but
+        # it returns 0. We'll just hold off on this.
+        #entities = text.annotations.count()
+        entities = Annotation.query.count()
         max_pages = int(math.ceil(entities / app.config['ANNOTATIONS_PER_PAGE']))
 
         rv = self.app.get(url)
@@ -288,32 +293,27 @@ class Test(unittest.TestCase):
 
 
     def test_tag(self):
-        self.setup_annotations()
-        self.setup_lines()
-        db.session.commit()
         tags = Tag.query.all()
         sorts = ['newest', 'oldest', 'weight', 'modified']
-
         for tag in tags:
-            with app.app_context():
-                url = url_for('tag', tag=tag.tag)
-            rv = self.app.get(url)
+            rv = self.app.get(f'https://www.example.com/tag/{tag.tag}')
             self.assertEqual(rv.status_code, 200)
-            entities = len(tag.annotations.all())
-            max_pages = int(math.ceil(entities/app.config['ANNOTATIONS_PER_PAGE']))
-            # This test doesn't work. The problem is simply that I can't
-            # properly query the number of annotations. The route works, the
-            # test just doesn't, so I have to fudge it. I am getting
-            # increasingly annoyed by this fact.
-            max_pages = 3
+
+            entities = tag.annotations.count()
+            self.assertTrue(entities > 0)
+            if entities == 1:
+                max_pages = 1
+            else:
+                max_pages = int(math.ceil(entities/app.config['ANNOTATIONS_PER_PAGE']))
             for sort in sorts:
                 rv = self.app.get(f'{url}?sort={sort}')
                 self.assertEqual(rv.status_code, 200)
                 rv = self.app.get(f'{url}?sort={sort}&page=1')
                 self.assertEqual(rv.status_code, 200)
                 rv = self.app.get(f'{url}?sort={sort}&page={max_pages+1}')
+                if rv.status_code == 200:
+                    print(rv.data)
                 self.assertEqual(rv.status_code, 404)
-
 
     def test_user_index(self):
         sorts = ['reputation', 'name', 'annotation', 'edits']
@@ -335,10 +335,6 @@ class Test(unittest.TestCase):
 
 
     def test_read(self):
-        self.setup_lines()
-        self.setup_annotations()
-        db.session.commit()
-
         text = Text.query.first()
         enum = LineEnum.query.first()
         lines = Line.query.filter_by(label=enum).all()
@@ -382,18 +378,12 @@ class Test(unittest.TestCase):
 
 
     def test_annotation_view(self):
-        self.setup_annotations()
-        db.session.commit()
-        
         for annotation in Annotation.query:
             with app.app_context():
                 url = url_for('annotation', annotation_id=annotation.id)
             self.assertEqual(self.app.get(url).status_code, 200)
 
     def test_edit_history(self):
-        self.setup_annotations()
-        db.session.commit()
-
         sorts = ['num', 'editor', 'time', 'reason']
 
         for annotation in Annotation.query:
@@ -408,11 +398,6 @@ class Test(unittest.TestCase):
                 self.assertEqual(self.app.get(f'{url}_invert').status_code, 200)
 
     def test_edit_view(self):
-        self.setup_annotations()
-        self.setup_lines()
-        db.session.commit()
-
-        # Annotation.edits and Annotation.HEAD are not working
         for annotation in Annotation.query:
             with app.app_context():
                 url = url_for('view_edit', annotation_id=annotation.id, num=0)
@@ -420,16 +405,12 @@ class Test(unittest.TestCase):
 
 
     def test_annotation_comments(self):
-        self.setup_annotations()
-        self.setup_lines()
-        db.session.commit()
         for annotation in Annotation.query:
             with app.app_context():
                 url = url_for('comments', annotation_id=annotation.id)
             self.assertEqual(self.app.get(url).status_code, 200)
 
     def test_annotate(self):
-        self.setup_lines()
         db.session.commit()
         number_of_anotations = len(Annotation.query.all())
         with self.app:
@@ -461,6 +442,9 @@ class Test(unittest.TestCase):
         rv = self.app.get(url)
         self.assertTrue(b"This is a test" in rv.data)
         
+    def test_secondaries(self):
+        text = Text.query.first()
+        self.assertTrue(text.annotations.count())
 
 
 if __name__ == '__main__':
