572e00d7a12f919e656d1ba72884aedea31a81d0
malan88 <malan88@standingwater.io>
2019-01-20 19:53:36 -0500

    Add some tests and fixtures; get ready to write a ton of tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,9 @@
-import pytest
+import pytest, os, yaml
 
 from icc import create_app, db
-from icc.models import classes, Text, Writer, Edition, LineEnum, Annotation,\
-        User
-from config import Config #dir_path = os.path.dirname(os.path.realpath(__file__))
-#fin = open(f'{dir_path}/data.yml', 'rt')
-#data = yaml.load(fin)
-#authors = data['text'].pop('authors')
-#edition = data['text'].pop('edition')
-#annotations = edition.pop('annotations')
-#lines = edition.pop('lines')
-#text = data.pop('text')
-#enums = data.pop('enums')
-#password = 'testing'
-#
+from icc.models import classes
+from config import Config
+
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     TESTING = True
@@ -21,13 +11,14 @@ class TestConfig(Config):
     #SERVER_NAME = 'www.testing.com'
     WTF_CSRF_ENABLED = 0
 
-@pytest.fixture(scope='session')
+@pytest.fixture
 def app():
     app = create_app(TestConfig)
 
     with app.app_context():
         db.create_all()
-        db.session.add(User(displayname='test', email='test@test.com'))
+        db.session.add(classes['User'](displayname='test',
+            email='test@test.com'))
         db.session.commit()
 
     yield app
@@ -36,6 +27,69 @@ def app():
         db.session.remove()
         db.drop_all()
 
-@pytest.fixture(scope='session')
+@pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def pop(app):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    fin = open(f'{dir_path}/data.yml', 'rt')
+    data = yaml.load(fin)
+
+    password = 'testing'
+    with app.app_context():
+        # populate all enums
+        for enum, enums in data['enums'].items():
+            for instance in enums:
+                obj = classes[enum](**instance)
+                if enum == 'User':
+                    obj.set_password(password)
+                db.session.add(obj)
+
+        # pop authors and edition from text
+        authors = data['text'].pop('authors')
+        edition = data['text'].pop('edition')
+
+        # populate the text
+        t = classes['Text'](**data['text'])
+        db.session.add(t)
+
+        # populate the authors
+        for author in authors:
+            t.authors.append(classes['Writer'](**author))
+
+        # pop annotations and lines from edition
+        annotations = edition.pop('annotations')
+        lines = edition.pop('lines')
+
+        # populate the edition
+        e = classes['Edition'](text=t, **edition)
+        db.session.add(e)
+
+        # popoulate the lines
+        labels = classes['LineEnum'].query.all()
+        label = {}
+        for l in labels:
+            label[f'{l.enum}>{l.display}'] = l
+        for line in lines:
+            db.session.add(classes['Line'](edition=e, num=line['num'],
+                label=label[line['enum']],
+                em_status=label[line['em_status']],
+                lvl1=line['l1'], lvl2=line['l2'], lvl3=line['l3'],
+                lvl4=line['l4'], line=line['line']))
+
+        # populate the annotations
+        for a in annotations:
+            annotator = a.pop('annotator')
+            annotator = classes['User'].query.filter_by(displayname=annotator).first()
+            tag_strings = a.pop('tags')
+            tags = [classes['Tag'].query.filter_by(tag=tag).first() for tag in tag_strings]
+            db.session.add(classes['Annotation'](annotator=annotator, edition=e, tags=tags,
+                **a))
+        db.session.commit()
+    return app 
+
+@pytest.fixture
+def popclient(pop):
+    return pop.test_client()
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,17 +1,19 @@
 from icc.models import User
 
-#def test_empty_db(client):
-#    """Start with a blank database."""
-#
-#    rv = client.get('/')
-#    assert b'html' in rv.data
-
-#def test_populated_db(client):
-#    rv = client.get('/')
-#    assert rv.status_code == 200
+def test_empty_db(client):
+    """Start with a blank database."""
+    rv = client.get('/')
+    assert b'html' in rv.data
 
 def test_user(app):
+    """Test a test user."""
     with app.app_context():
         u = User.query.first()
     assert u.displayname == 'test'
     assert u.email == 'test@test.com'
+
+def test_populated_db(popclient):
+    """Test populating the database."""
+    rv = popclient.get('/')
+    assert rv.status_code == 200
+
