19e075ea080561dfccb424cf4b9eff64187a9faa
malan88 <malan88@standingwater.io>
2019-01-23 17:54:41 -0500

    Finally get tests working consistently.

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,6 @@ class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     TESTING = True
     ELASTICSEARCH_URL = None
-    WTF_CSRF_ENABLED = 0
 
 
 # create the app; this is the fundamental fixture from which all is created. The
@@ -21,13 +20,9 @@ def app():
 
     with app.app_context():
         db.create_all()
-        db.session.add(classes['User'](displayname='test',
-            email='test@test.com'))
-        db.session.commit()
 
-    yield app
+        yield app
 
-    with app.app_context():
         db.session.remove()
         db.drop_all()
 
@@ -46,56 +41,55 @@ def pop(app):
     data = yaml.load(fin)
 
     password = 'testing'
-    with app.app_context():
-        # populate all enums
-        for enum, enums in data['enums'].items():
-            for instance in enums:
-                obj = classes[enum](**instance)
-                if enum == 'User':
-                    obj.set_password(password)
-                db.session.add(obj)
+    # populate all enums
+    for enum, enums in data['enums'].items():
+        for instance in enums:
+            obj = classes[enum](**instance)
+            if enum == 'User':
+                obj.set_password(password)
+            db.session.add(obj)
 
-        # pop authors and edition from text
-        authors = data['text'].pop('authors')
-        edition = data['text'].pop('edition')
+    # pop authors and edition from text
+    authors = data['text'].pop('authors')
+    edition = data['text'].pop('edition')
 
-        # populate the text
-        t = classes['Text'](**data['text'])
-        db.session.add(t)
+    # populate the text
+    t = classes['Text'](**data['text'])
+    db.session.add(t)
 
-        # populate the authors
-        for author in authors:
-            t.authors.append(classes['Writer'](**author))
+    # populate the authors
+    for author in authors:
+        t.authors.append(classes['Writer'](**author))
 
-        # pop annotations and lines from edition
-        annotations = edition.pop('annotations')
-        lines = edition.pop('lines')
+    # pop annotations and lines from edition
+    annotations = edition.pop('annotations')
+    lines = edition.pop('lines')
 
-        # populate the edition
-        e = classes['Edition'](text=t, **edition)
-        db.session.add(e)
+    # populate the edition
+    e = classes['Edition'](text=t, **edition)
+    db.session.add(e)
 
-        # popoulate the lines
-        labels = classes['LineEnum'].query.all()
-        label = {}
-        for l in labels:
-            label[f'{l.enum}>{l.display}'] = l
-        for line in lines:
-            db.session.add(classes['Line'](edition=e, num=line['num'],
-                label=label[line['enum']],
-                em_status=label[line['em_status']],
-                lvl1=line['l1'], lvl2=line['l2'], lvl3=line['l3'],
-                lvl4=line['l4'], line=line['line']))
+    # popoulate the lines
+    labels = classes['LineEnum'].query.all()
+    label = {}
+    for l in labels:
+        label[f'{l.enum}>{l.display}'] = l
+    for line in lines:
+        db.session.add(classes['Line'](edition=e, num=line['num'],
+            label=label[line['enum']],
+            em_status=label[line['em_status']],
+            lvl1=line['l1'], lvl2=line['l2'], lvl3=line['l3'],
+            lvl4=line['l4'], line=line['line']))
 
-        # populate the annotations
-        for a in annotations:
-            annotator = a.pop('annotator')
-            annotator = classes['User'].query.filter_by(displayname=annotator).first()
-            tag_strings = a.pop('tags')
-            tags = [classes['Tag'].query.filter_by(tag=tag).first() for tag in tag_strings]
-            db.session.add(classes['Annotation'](annotator=annotator, edition=e, tags=tags,
-                **a))
-        db.session.commit()
+    # populate the annotations
+    for a in annotations:
+        annotator = a.pop('annotator')
+        annotator = classes['User'].query.filter_by(displayname=annotator).first()
+        tag_strings = a.pop('tags')
+        tags = [classes['Tag'].query.filter_by(tag=tag).first() for tag in tag_strings]
+        db.session.add(classes['Annotation'](annotator=annotator, edition=e, tags=tags,
+            **a))
+    db.session.commit()
     return app 
 
 
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,19 +1,57 @@
-from icc.models import User
+import re
+from icc.models import classes
+
+
+def get_token(data):
+    m = re.search(b'<input id="csrf_token" name="csrf_token" type="hidden" value="(.*)">', data)
+    return m.group(1).decode("utf-8")
+
 
 def test_empty_db(client):
     """Start with a blank database."""
     rv = client.get('/')
     assert b'html' in rv.data
 
-def test_user(app):
-    """Test a test user."""
-    with app.app_context():
-        u = User.query.first()
-    assert u.displayname == 'test'
-    assert u.email == 'test@test.com'
 
-def test_populated_db(popclient):
-    """Test populating the database."""
+def test_index(popclient):
     rv = popclient.get('/')
-    assert rv.status_code == 200
+    assert b'<annotation ' in rv.data
 
+
+def test_register(client):
+    """Test user registration"""
+    url = '/user/register'
+    rv = client.get(url)
+    assert rv.status_code == 200
+    rv = client.post(url,
+            data={
+                'displayname': 'tester', 
+                'email': 'george@test.com',
+                'password': 'test',
+                'password2': 'test',
+                'csrf_token': get_token(rv.data)
+                },
+            follow_redirects=True)
+    assert rv.status_code == 200
+    rv = client.get('user/list')
+    assert b'tester' in rv.data
+
+
+def test_login_logout(popclient):
+    """Test login and logout."""
+    url = '/user/login'
+    rv = popclient.get(url)
+    assert rv.status_code == 200
+    for l in rv.data.split(b'\n'):
+        if b'csrf_token' in l:
+            print(l)
+    print(f"Get method: {get_token(rv.data)}")
+    rv = popclient.post(url,
+            data={
+                'email': 'george@example.com',
+                'password': 'testing',
+                'csrf_token': get_token(rv.data)
+                },
+            follow_redirects=True)
+    assert rv.status_code == 200
+    assert b'logout' in rv.data
