4b0c5ea0cb1231a8de0c736d32a71ffb5f0bb803
malan88 <malan88@standingwater.io>
2019-01-24 12:56:47 -0500

    flake8 compliance for ajax

--- icc/ajax/routes.py
+++ icc/ajax/routes.py
@@ -1,14 +1,14 @@
 from flask import request, jsonify
-from icc import db
 from icc.models import Tag
 from icc.ajax import ajax
+
 
 @ajax.route('/autocomplete/tags/', methods=['POST'])
 def ajax_tags():
     tagstr = request.form.get('tags')
     tags = tagstr.split()
-    results = Tag.query.filter(Tag.tag.startswith(tags[-1]), Tag.locked==False)\
-            .limit(6)
+    results = Tag.query.filter(Tag.tag.startswith(tags[-1]),
+                               Tag.locked==False).limit(6)
     print(results)
     if not results:
         return jsonify({'success': False, 'tags': []})
@@ -18,4 +18,5 @@ def ajax_tags():
         tag_list.append(t.tag)
         descriptions.append(t.wiki.current.body)
 
-    return jsonify({'success': True, 'tags': tag_list, 'descriptions': descriptions })
+    return jsonify({'success': True, 'tags': tag_list,
+                    'descriptions': descriptions})
