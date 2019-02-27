654956584f886d14f4c9f84d5c7093d7990357da
malan88 <malan88@standingwater.io>
2019-02-27 12:32:08 -0500

    Expand documentation for models.user

--- icc/models/user.py
+++ icc/models/user.py
@@ -26,13 +26,27 @@ import icc.models.annotation
 
 
 @login.user_loader
-def load_user(id):
-    """Necessary method for flask_login."""
+def _load_user(id):
+    """Necessary method for flask_login. Do not call.
+
+    Parameters
+    ----------
+    id : int
+        The id of the User
+
+    Returns
+    -------
+    :class:`User`
+    """
     return User.query.get(int(id))
 
 
 class User(UserMixin, Base):
     """The User class.
+
+    Inherits
+    --------
+    UserMixin
 
     Attributes
     ----------
@@ -63,13 +77,12 @@ class User(UserMixin, Base):
         A list of all of the :class:`AdminRight` objects the user has.
     annotations : BaseQuery
         An SQLA BaseQuery of all the annotations the user has authored.
-    voted_<class> : list
-        A list of <class> objects that have been voted on by the user. I am
-        going to work to make this more dynamic and less anti-DRY
     followed_<class>s : BaseQuery
         A BaseQuery of all of the <class>'s that the user is currently
         following. The <class> is the class's name lowercased (same as the table
         name).
+    followers : BaseQuery
+        A BaseQuery of all of the User's that follow this User.
     """
     displayname = db.Column(db.String(64), index=True)
     email = db.Column(db.String(128), index=True, unique=True)
@@ -82,12 +95,8 @@ class User(UserMixin, Base):
     rights = db.relationship('AdminRight', secondary='rights')
     annotations = db.relationship('Annotation', lazy='dynamic')
 
-    voted_annotation = association_proxy('annotationvote_ballots', 'annotation')
-    voted_edit = association_proxy('edit_ballots', 'edit')
-    voted_wikiedit = association_proxy('wikiedit_ballots', 'edit')
-    voted_textrequest = association_proxy('textrequest_ballots', 'request')
-    voted_tagrequest = association_proxy('tagrequest_ballots', 'request')
-
+    # Because this is a self-referential many-to-many it is defined explicitly
+    # as opposed to using my FollowableMixin
     followed_users = db.relationship(
         'User',
         secondary=db.Table(
@@ -100,10 +109,15 @@ class User(UserMixin, Base):
 
     @property
     def url(self):
+        """A string that represents the url for this user object (i.e., the
+        user's profile page.
+        """
         return url_for("user.profile", user_id=self.id)
 
     @property
     def readable_reputation(self):
+        """A string that represents a nicely formatted reputation of this user.
+        """
         if self.reputation >= 1000000:
             return f'{round(self.reputation/1000000)}m'
         elif self.reputation >= 1000:
@@ -112,11 +126,19 @@ class User(UserMixin, Base):
             return f'{self.reputation}'
 
     def avatar(self, size):
+        """A link to the gravatar for this user. I will probably eventually want
+        to simply eliminate avatars. Especially gravatars as they are a security
+        vulnerability.
+        """
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
     @property
     def up_power(self):
+        """An int that represents the user's current upvote power.
+
+        This is currently set to 10log10 of the user's reputation, floored at 1.
+        """
         if self.reputation <= 1:
             return 1
         else:
@@ -124,6 +146,9 @@ class User(UserMixin, Base):
 
     @property
     def down_power(self):
+        """An int of the user's down power. This is simply half of the user's up
+        power, but at least one.
+        """
         power = self.up_power
         if power / 2 <= 1:
             return -1
@@ -134,26 +159,79 @@ class User(UserMixin, Base):
         return f"<User {self.displayname}>"
 
     def update_last_seen(self):
+        """A method that will update the user's last seen timestamp."""
         self.last_seen = datetime.utcnow()
 
     def flag(self, flag, thrower):
-        event = UserFlag(flag=flag, user=self, thrower=thrower)
-        db.session.add(event)
+        """A method to flag the user.
+
+        Parameters
+        ----------
+        flag : str
+            A string that corresponds to the flag to flag the user with.
+        thrower : :class:`User`
+            A User object of the user throwing the flag. (i.e., the snitch 😜)
+        """
+        flag_obj = UserFlag(flag=flag, user=self, thrower=thrower)
+        db.session.add(flag_obj)
 
     # Password routes
     def set_password(self, password):
+        """Set the password for the user.
+
+        Parameters
+        ----------
+        password : str
+            The user's password.
+        """
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
+        """Check that the password provided is the user's current password.
+
+        Parameters
+        ----------
+        password : str
+            The user's password
+
+        Returns
+        -------
+        bool
+            Whether the password is correct.
+        """
         return check_password_hash(self.password_hash, password)
 
     def get_reset_password_token(self, expires_in=600):
+        """Generate a reset_password token for the user's emailed link to reset
+        the password.
+
+        Parameters
+        ----------
+        expires_in : int
+            The number of seconds until the token expires. Defaults to 600
+
+        Returns
+        ------
+        jwt-token
+            The token for the reset-link.
+        """
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
             app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
 
     @staticmethod
     def verify_reset_password_token(token):
+        """A static method to verify a reset password token's validity.
+
+        Parameters
+        ----------
+        token : jwt-token
+            The token to be checked.
+
+        Returns
+        :class:`User`
+            The user. If the token is valid.
+        """
         try:
             id = jwt.decode(token, app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
@@ -162,26 +240,67 @@ class User(UserMixin, Base):
         return User.query.get(id)
 
     # admin authorization methods
-    def authorize(self, right):
-        if self.is_authorized(right):
-            pass
-        else:
-            abort(403)
-
     def is_authorized(self, right):
+        """Check if a user is authorized with a particular right.
+
+        Parameters
+        ----------
+        right : str
+            The right.
+
+        Returns
+        -------
+        bool
+            Whether the user is authorized.
+        """
         r = AdminRight.query.filter_by(enum=right).first()
         print(r)
         print(r.min_rep)
         print(self.reputation)
         return r in self.rights or (r.min_rep and self.reputation >= r.min_rep)
 
+    def authorize(self, right):
+        """Authorize a user with a right.
+
+        Parameters
+        ----------
+        right : str
+            A string corresponding to the right enum being authorized.
+
+        Notes
+        -----
+        This is different from is_authorized (in fact, it uses is_authorized):
+        this method will throw a 403 abort if the user is not authorized. It
+        simplifies some logic.
+        """
+        if self.is_authorized(right):
+            pass
+        else:
+            abort(403)
+
     def get_vote(self, obj):
         """Get the vote on the object. If the object does not have a
         __vote__ attribute, it's not going to work and a TypeError will be
         raised.
 
-        This method works for every votable class and can be tested as a bool
+        This method works for every votable class and can be tested as a Falsey
         for whether or whether not the user has voted on the object.
+
+        Parameters
+        ----------
+        obj : any votable object
+            The object we're looking for the vote on.
+
+        Returns
+        -------
+        <class>Vote
+            The vote on the object.
+
+        Raises
+        ------
+        TypeError
+            If the object doesn't have a valid __vote__ attribute corresponding
+            to the object's vote class.
         """
         if not obj.__vote__:
             raise TypeError("The requested object is missing an `__vote__` "
@@ -191,6 +310,20 @@ class User(UserMixin, Base):
 
 
 class AdminRight(Base, EnumMixin):
+    """The class used to represent a user's rights.
+
+    Inherits
+    --------
+    EnumMixin
+
+    Attributes
+    ----------
+    min_rep : int
+        An integer representing the minimum reputation to authorize a user for
+        the right (so we grant certain rights based on the user's reputation).
+        If the min_rep is None, the user has to posses the right in their
+        `rights`.
+    """
     min_rep = db.Column(db.Integer)
 
     def __repr__(self):
@@ -198,10 +331,41 @@ class AdminRight(Base, EnumMixin):
 
 
 class ReputationEnum(Base, EnumMixin):
+    """An enum for a ReputationChange.
+
+    Inherits
+    --------
+    EnumMixin
+
+    Attributes
+    ----------
+    default_delta : int
+        An integer representing the default reputation change value. That is to
+        say, the amount by which the event will change the user's reputation.
+    """
     default_delta = db.Column(db.Integer, nullable=False)
 
 
 class ReputationChange(Base):
+    """An object representing a reputation change event. This thing exists to
+    avoid mischanges to the reputation. It's a ledger so we can audit every rep
+    change and the user can see the events.
+
+    Attributes
+    ----------
+    delta : int
+        A number representing the change to the user's reputation.
+    user_id : int
+        The id of the user.
+    timestamp : datetime
+        The time the reputation change happened
+    enum_id : int
+        the id of the reputation change enum
+    user : :class:`User`
+        The user whose reputation was changed.
+    enum : :class:`ReputationEnum`
+        The enum of the particular rep change.
+    """
     delta = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
@@ -209,33 +373,35 @@ class ReputationChange(Base):
                         nullable=False)
 
     user = db.relationship('User', backref='changes')
-    type = db.relationship('ReputationEnum')
+    enum = db.relationship('ReputationEnum')
 
     def __repr__(self):
         return f'<rep change {self.type} on {self.user.displayname}>'
 
 
 class UserFlagEnum(Base, EnumMixin):
-    ...
+    """An enum class for user flags."""
 
 
 class UserFlag(Base):
-    user_flag_id = db.Column(db.Integer, db.ForeignKey('userflagenum.id'),
-                             index=True)
-
+    enum_id = db.Column(db.Integer, db.ForeignKey('userflagenum.id'),
+                        index=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)
-
     thrower_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)
     time_thrown = db.Column(db.DateTime, default=datetime.utcnow())
-
     time_resolved = db.Column(db.DateTime)
     resolver_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)
 
     user = db.relationship('User', foreign_keys=[user_id])
     thrower = db.relationship('User', foreign_keys=[thrower_id])
     resolver = db.relationship('User', foreign_keys=[resolver_id])
-
     flag = db.relationship('UserFlagEnum')
+
+    def __init__(self, *args, **kwargs):
+        flag = kwargs.pop('flag')
+        super().__init__(*args, **kwargs)
+        flag_enum = UserFlagEnum.query.filter(enum=flag).first()
+        self.flag = flag_enum
 
     def __repr__(self):
         if self.resolved:
