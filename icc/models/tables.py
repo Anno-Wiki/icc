de9b43010c383be8a0556156e268eb551baff8ca
malan88 <malan88@standingwater.io>
2019-02-28 08:50:42 -0500

    Document models.tables

--- icc/models/tables.py
+++ icc/models/tables.py
@@ -1,11 +1,15 @@
+"""Basic many-to-many tables."""
 from icc import db
 
 
+"""The tags table connects tags to edits."""
 tags = db.Table(
     'tags',
     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
-    db.Column('edit_id', db.Integer, db.ForeignKey('edit.id',
-                                                   ondelete='CASCADE')))
+    db.Column('edit_id', db.Integer, db.ForeignKey(
+        'edit.id', ondelete='CASCADE')))
+
+"""The rights table connects AdminRight to a User."""
 rights = db.Table(
     'rights',
     db.Column('right_id', db.Integer, db.ForeignKey('adminright.id')),
