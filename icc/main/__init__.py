f510c8c1ddff123f133302fc74dc44603f85f3d3
malan88 <malan88@standingwater.io>
2019-03-21 12:29:57 -0400

    Move main templates to main
    All of the templates that are only used by main have been moved to the
    main blueprint. So the templates folder has become very light.
    
    I also cleaned up the user index template.

--- icc/main/__init__.py
+++ icc/main/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-main = Blueprint("main", __name__, template_folder="../templates")
+main = Blueprint("main", __name__, template_folder="templates")
 
 from icc.main import annotations, editions, routes, tags, texts, wikis, writers
