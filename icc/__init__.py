b84def3564aedad3f71d2ae23e1486a77157c7f9
malan88 <malan88@standingwater.io>
2019-04-20 07:53:50 -0400

    Eliminate useless whitespace in jinja2

--- icc/__init__.py
+++ icc/__init__.py
@@ -84,6 +84,8 @@ def create_app(config_class=Config):
     app.jinja_env.globals['enumerate'] = enumerate
     from icc.funky import proc_links
     app.jinja_env.filters['proc_links'] = proc_links
+    app.jinja_env.trim_blocks = True
+    app.jinja_env.lstrip_blocks = True
 
     return app
 
