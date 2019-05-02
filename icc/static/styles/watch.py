d6f627e76063c73f6ed6ea2180d71000c1f36dd0
malan88 <malan88@standingwater.io>
2019-05-02 11:46:41 -0400

    Add a Sass watch jury-rig script

---
+++ icc/static/styles/watch.py
@@ -0,0 +1,13 @@
+from os import stat
+from subprocess import call
+from time import sleep, time, localtime, asctime
+
+filename = "style.scss"
+last_seen = stat(filename).st_mtime
+
+while True:
+    if last_seen < stat(filename).st_mtime:
+        call(['sass', 'style.scss', 'style.css'])
+        last_seen = stat(filename).st_mtime
+        print(f"Recompiled {asctime(localtime(time()))}")
+    sleep(1)
