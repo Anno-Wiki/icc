de93b66e3d2da3ac29bbee4cf9c53a4d786ade37
malan88 <malan88@standingwater.io>
2019-05-04 10:10:25 -0400

    Modularize the styles, update watch as a consequence

--- icc/static/styles/watch.py
+++ icc/static/styles/watch.py
@@ -1,13 +1,19 @@
-from os import stat
+from os import stat, walk
 from subprocess import call
 from time import sleep, time, localtime, asctime
 
-filename = "style.scss"
-last_seen = stat(filename).st_mtime
+
+last_seen = 0
+for root, dirs, files in walk('.'):
+    for name in files:
+        if last_seen < stat(name).st_mtime:
+            last_seen = stat(name).st_mtime
 
 while True:
-    if last_seen < stat(filename).st_mtime:
-        call(['sass', 'style.scss', 'style.css'])
-        last_seen = stat(filename).st_mtime
-        print(f"Recompiled {asctime(localtime(time()))}")
-    sleep(0.1)
+    for root, dirs, files in walk('.'):
+        for file in files:
+            if last_seen < stat(name).st_mtime:
+                call(['sass', '-C', 'style.scss', 'style.css'])
+                last_seen = stat(name).st_mtime
+                print(f"Recompiled {asctime(localtime(time()))}")
+            sleep(0.1)
