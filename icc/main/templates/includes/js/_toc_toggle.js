e99856ecd2b0fe57da1b0ee52e8d4e2d5fea76b6
malan88 <malan88@standingwater.io>
2019-05-05 10:58:58 -0400

    Clean _toc_toggle.js

--- icc/main/templates/includes/js/_toc_toggle.js
+++ icc/main/templates/includes/js/_toc_toggle.js
@@ -1,14 +1,15 @@
 <script>
     function toggle(x) {
         cls = x.parentNode.parentNode.classList[1];
-        elements = document.getElementsByClassName(cls);
+        elements = allof(cls);
+
         if (elements[1].style.display == "" || elements[1].style.display == "none") {
-            for (var i = 1; i < elements.length; i++) {
+            for (let i = 1; i < elements.length; i++) {
                 elements[i].style.display = "table-row";
             }
             x.innerHTML = "[ - ]"
         } else {
-            for (var i = 1; i < elements.length; i++) {
+            for (let i = 1; i < elements.length; i++) {
                 elements[i].style.display = "none";
             }
             x.innerHTML = "[ + ]"
