14bab66293b97ded4cb990eefda67c17557650c1
malan88 <malan88@standingwater.io>
2019-05-29 16:09:25 -0400

    Use SSL env var

--- config.py
+++ config.py
@@ -22,6 +22,7 @@ class Config:
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') is not None
 
     # vote margins
     VOTES_FOR_APPROVAL = 2
