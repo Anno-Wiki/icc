ee1ef5bfbf445fad824c078f9b41001dd45c41bb
malan88 <malan88@standingwater.io>
2019-06-07 13:09:16 -0400

    For some reason the password insert wasn't working

--- data/scripts/repopulate.sh
+++ data/scripts/repopulate.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-echo -n MySQL password:
-read -s mysqlpw
-printf '\n'
-
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 sudo echo "- dropping and recreating database..."
-echo "drop database icc; create database icc;" |\
-    sudo mysql -u root --password=$mysqlpw icc
+
+echo "drop database icc; create database icc;" | mysql icc
+
 echo "- recreating table structure..."
 $ICCVENV/bin/flask db upgrade
 $DIR/populate.sh
