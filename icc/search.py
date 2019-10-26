bb1e2ed7a33e1a477a88c243defaa1d1b7c6c886
malan88 <malan88@standingwater.io>
2019-10-26 14:17:01 -0400

    Fix primary search

--- icc/search.py
+++ icc/search.py
@@ -57,10 +57,16 @@ def query_index(index, query, page, per_page):
     """Search the index."""
     if not current_app.elasticsearch:
         return [], 0
+    body = {
+        'query': {'multi_match':
+                  {'query': query, 'fields': ['body']}
+                  },
+        'from': (page - 1) * per_page,
+        'size': per_page}
+
     search = current_app.elasticsearch.search(
-        index=index, doc_type=index,
-        body={'query': {'multi_match': {'query': query, 'fields': ['*']}},
-              'from': (page - 1) * per_page, 'size': per_page})
+        index=index, doc_type=index, body=body)
+
     ids = [int(hit['_id']) for hit in search['hits']['hits']]
     return ids, search['hits']['total']
 
