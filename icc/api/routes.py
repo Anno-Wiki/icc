265121874dae42a0679fe80ec994e7c8bead2616
malan88 <m@stdwtr.io>
2020-07-04 15:28:17 -0400

    Add login api route skeleton

--- icc/api/routes.py
+++ icc/api/routes.py
@@ -1,6 +1,15 @@
 import time
+
+from flask import request
+
 from icc.api import api
 
 @api.route('/time')
 def get_time():
     return {'time': time.time()}
+
+@api.route('/login', methods=['POST'])
+def login():
+    print('HELLO')
+    print(request.data)
+    return request.data
