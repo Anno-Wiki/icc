90686e82ae70c731875d040cb34f88c902c58b78
malan88 <m@stdwtr.io>
2020-07-05 15:09:27 -0400

    Login properly, add current_user testing route

--- icc/api/routes.py
+++ icc/api/routes.py
@@ -1,8 +1,11 @@
 import time
 
 from flask import request
+from flask_login import current_user, login_user
 
 from icc.api import api
+
+from icc.models.user import User
 
 @api.route('/time')
 def get_time():
@@ -10,6 +13,18 @@ def get_time():
 
 @api.route('/login', methods=['POST'])
 def login():
-    print('HELLO')
-    print(request.data)
-    return request.data
+    user = User.query.filter_by(email=request.json['email']).first()
+
+    if user and user.locked:
+        return {'success': False, 'flash': "That account is locked."}
+    elif user is None or not user.check_password(request.json['password']):
+        return {'success': False, 'flash': "Invalid email or password"}
+
+    return {'success': True}
+
+@api.route('/current_user')
+def get_current_user():
+    if current_user.is_authenticated:
+        return {'user': current_user.displayname}
+    else:
+        return {'user': None}
